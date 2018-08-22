38639f28e8588a77a4da25f60e905db4042c3723
undernotic <undernotic@gmail.com>
2018-08-22 20:03:51 +0200

    fixing old npm modules, better example bage

--- example/src/App.js
+++ example/src/App.js
@@ -1,67 +1,85 @@
 import React, { Component } from "react";
 import "./App.css";
 import logo from "./logo.png";
+import mainGif from "./main.gif";
 import HighlightedCode from "./HighlightedCode";
 import ReactLoadComponent from "react-file-load";
 
 class App extends Component {
   render() {
     let example1Text = `<ReactLoadComponent text="Click me to check progress bar!"/>`;
-    let example2Text = `<ReactLoadComponent text="Click me and provide comma seperated file!" readeryConfig={{ splitBy: ",", encoding: "UTF-8" }} onDataChunk={i => console.log("Im item from csv file " + i)}/>`;
-    let example3Text = `<ReactLoadComponent text="Custom colors" fromColor = "yellow" toColor="black" finishedColor="green" style={{backgroundColor: "grey", color: "red", width: "400px"}}/>`;
+    let example2Text = `<ReactLoadComponent text="Click me and provide comma seperated file!" 
+    readeryConfig={{ splitBy: ",", encoding: "UTF-8" }} 
+    onDataChunk={i => console.log("Im item from csv file " + i)}/>`;
+    let example3Text = `<ReactLoadComponent text="Custom colors" 
+    fromColor = "yellow" 
+    toColor="black" 
+    finishedColor="green" 
+    style={{backgroundColor: "grey", color: "red", width: "400px"}}/>`;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <img src={mainGif} className="App-gif" alt="git" />
         </header>
         <hr />
-        <div className="Flex-container">
-          <div style={{ marginTop: "150px",  marginRight: "10px" }}>
-            <ReactLoadComponent text="Click me to check progress bar!" />
-          </div>
-          <div style={{width: "70%"}}>
-            <h2>Progress bar</h2>
-            <HighlightedCode>{example1Text}</HighlightedCode>;  
-          </div>
-        </div>
-        <hr />
 
-        <div className="Flex-container">
-          <div style={{ marginTop: "150px", marginRight: "10px" }}>
-            <ReactLoadComponent
-              text="Click me and provide comma seperated file!"
-              readeryConfig={{ splitBy: ",", encoding: "UTF-8" }}
-              onDataChunk={i => console.log("Im item from csv file" + i)}
-            />
-          </div>
+        <div>
+          <h2>Progress bar -></h2>
+          <div className="Flex-container">
+            <div>
+              <ReactLoadComponent text="Click me to check progress bar!" />
+            </div>
+            &rArr;
           <div>
-            <h2>Read data in chunks</h2>
-            <p>
-
-            </p>
+              <HighlightedCode >{example1Text}</HighlightedCode>
+            </div>
           </div>
         </div>
+
         <hr />
 
-        <div className="Flex-container">
-          <div style={{ marginTop: "160px", marginRight: "10px" }}>
-            <ReactLoadComponent
-              text="Custom colors"
-              fromColor="yellow"
-              toColor="black"
-              finishedColor="green"
-              style={{ backgroundColor: "grey", color: "red", width: "400px" }}
-            />
-          </div>
+        <div>
+          <h2>Read data in chunks</h2>
+          <div className="Flex-container">
+            <div>
+              <ReactLoadComponent
+                text="Click me and provide file!"
+                readeryConfig={{ splitBy: ",", encoding: "UTF-8" }}
+                onDataChunk={i => console.log("Im item from csv file" + i)}
+                finishedColor="green"
+              />
+            </div>
+            &rArr;
           <div>
-            <h2>Highly customizable</h2>
-            <p>
-              
-            </p>
+              <HighlightedCode>{example2Text}</HighlightedCode>
+            </div>
           </div>
         </div>
 
         <hr />
+
+        <div>
+          <h2>Highly customizable</h2>
+          <div className="Flex-container">
+            <div>
+              <ReactLoadComponent
+                text="Custom colors"
+                fromColor="yellow"
+                toColor="black"
+                finishedColor="green"
+                style={{ backgroundColor: "grey", color: "red", width: "400px" }}
+              />
+            </div>
+            &rArr;
+          <div>
+              <HighlightedCode>{example3Text}</HighlightedCode>;
+          </div>
+          </div>
+        </div>
+
+        <hr />
+        
         <br />
         <div className=" Link">
           <h2>
